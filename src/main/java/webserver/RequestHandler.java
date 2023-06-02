e8cd20a609d8e7793ca5ffe7347cea30424af030
Sugil Kim <sapzilking@gmail.com>
2023-06-03 06:00:11 +0900

    요구사항7 - CSS 지원하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -49,7 +49,6 @@ public class RequestHandler extends Thread {
                     logined = isLogin(line);
                 }
             }
-
             String url = tokens[1];
 
             if (("/user/create").equals(url)) {
@@ -102,6 +101,11 @@ public class RequestHandler extends Thread {
                 byte[] body = sb.toString().getBytes();
                 DataOutputStream dos = new DataOutputStream(out);
                 response200Header(dos, body.length);
+                responseBody(dos, body);
+            } else if (url.endsWith(".css")) {
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+                response200CssHeader(dos, body.length);
                 responseBody(dos, body);
             } else {
                 responseResource(out, url);
@@ -174,5 +178,16 @@ public class RequestHandler extends Thread {
             return false;
         }
         return Boolean.parseBoolean(value);
+    }
+
+    private void response200CssHeader(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/css\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
 }
